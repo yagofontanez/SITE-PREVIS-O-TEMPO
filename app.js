f80281bd05e17399d21ebe562f00fbadd96e9a49
Yago Fontanez <142320080+yagofontanez@users.noreply.github.com>
2023-11-11 16:09:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const apiKey = 'c3828d0b5a4485ab662cf88ad9997d1d';
+
+function consultarClima() {
+  const cidadeInput = document.getElementById('cidade');
+  const cidade = cidadeInput.value;
+
+  if (!cidade) {
+    alert('Por favor, digite o nome da cidade.');
+    return;
+  }
+
+  const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${apiKey}`;
+
+  obterDadosClima(apiUrl);
+}
+
+async function obterDadosClima(apiUrl) {
+  try {
+    const resposta = await axios.get(apiUrl);
+    const dadosClima = resposta.data;
+
+    
+    const weatherInfoContainer = document.getElementById('weather-info');
+    weatherInfoContainer.innerHTML = `
+      <p>Cidade: ${dadosClima.name}</p>
+      <p>Temperatura: ${dadosClima.main.temp}°C</p>
+      <p>Condição do tempo: ${dadosClima.weather[0].description}</p>
+    `;
+  } catch (erro) {
+    console.error('Erro ao obter dados do clima:', erro.message);
+  }
+}
